d9f066e2f167ee7a30344282875f70266f2c3313
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2023-11-15 11:29:20 +0200

    safsa

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('welcome', compact('posts'));
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,25 @@
 @extends('partials.layout')
 @section('content')
     <div class="container mx-auto">
-        <button class="btn btn-primary">Click</button>
+
+        <div class="flex flex-row flex-wrap ">
+
+            @foreach($posts as $post)
+                <div class="basis-1/4 mb-5">
+                    <div class="card bg-base-200 shadow-xl min-h-full mx-2">
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $post->title }}</h2>
+                            <p>{{ $post->snippet }}...</p>
+                            <p class="text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
+
+        </div>
     </div>
 @endsection
