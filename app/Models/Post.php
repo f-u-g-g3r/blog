c4e4fe9fe585ec990c24ab9ff163f54e042ebe51
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2024-01-29 22:15:21 +0200

    wuh

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,48 +1,48 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-
 class Post extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['title', 'body', 'image'];
-
-    public function snippet(): Attribute
-    {
-        return Attribute::get(function () {
+    protected $fillable = ['title', 'body'];
+    public function snippet():Attribute  {
+        return Attribute::get(function (){
             return substr($this->body, 0, 200);
         });
     }
-
-    public function user() {
+    public function user(){
         return $this->belongsTo(User::class);
     }
-
-    public function image() {
-        return $this->hasOne(Image::class);
+    public function images(){
+        return $this->hasMany(Image::class);
     }
 
-//    public function image(): Attribute
-//    {
-//        return Attribute::make(get: function ($image) {
-//
-//            if (!$image || parse_url($image, PHP_URL_SCHEME)) {
-//                return $image;
-//            }
-//            return Storage::url($image);
-//        },set: function($image) {
-//            if (!is_a($image, UploadedFile::class)) {
-//                return $image;
-//            }
-//            return request()->file('image')->store('public');
-//        }
-//        );
-//    }
+    // public function image():Attribute {
+    //     return Attribute::make(
+    //         get: function ($image){
+    //             if(!$image || parse_url($image, PHP_URL_SCHEME)){
+    //                 return $image;
+    //             }
+    //             return Storage::url($image);
+    //         },
+    //         set: function ($image){
+    //             if(!is_a($image, UploadedFile::class)){
+    //                 return $image;
+    //             }
+    //             return request()->file('image')->store('public');
+    //         }
+    //     );
+    // }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
-
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,21 +15,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
+Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
+Route::post('/post/{post}', [PublicController::class, 'comment'])->name('comment');
+Route::get('/post/{post}/like', [PublicController::class, 'like'])->name('like');
 
-//Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
-//Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
-//Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store');
-//Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-//Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-//Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-
-
+// Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 Route::middleware('auth')->group(function () {
     Route::resource('/admin/posts', PostController::class);
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 require __DIR__.'/auth.php';
