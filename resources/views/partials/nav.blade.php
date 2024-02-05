31851b254488a90e023162433aa192ea5588d024
Artjom Kuznetsov <artjom.kuznetsov2@tptlive.ee>
2024-02-05 15:29:58 +0200

    wersa

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,18 +2,19 @@
     <div class="navbar-start">
         <div class="dropdown">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
             </label>
             <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                 <li><a href="{{route('home')}}">Home</a></li>
-                @auth
                 <li>
                     <a>Admin</a>
                     <ul class="p-2">
                         <li><a href="{{route('posts.index')}}">Posts</a></li>
                     </ul>
                 </li>
-                @endauth
             </ul>
         </div>
         <a class="btn btn-ghost normal-case text-xl">{{config('app.name')}}</a>
@@ -22,35 +23,33 @@
         <ul class="menu menu-horizontal px-1">
             <li><a href="{{route('home')}}">Home</a></li>
             @auth
-            <li tabindex="0">
-                <details>
-                    <summary>Admin</summary>
-                    <ul class="p-2">
-                        <li><a href="{{route('posts.index')}}">Posts</a></li>
-                    </ul>
-                </details>
-            </li>
+                <li><a href="{{route('feed')}}">Feed</a></li>
+                <li tabindex="0" class="z-10">
+                    <details>
+                        <summary>Admin</summary>
+                        <ul class="p-2">
+                            <li><a href="{{route('posts.index')}}">Posts</a></li>
+                        </ul>
+                    </details>
+                </li>
             @endauth
         </ul>
     </div>
     <div class="navbar-end">
         @guest
-        <a class="btn btn-secondary" href="{{route('login')}}">Login</a>
-        <a class="btn btn-primary ml-3" href="{{route('register')}}">Register</a>
+            <a class="btn" href="{{route('login')}}">Login</a>
+            <a class="btn btn-primary ml-3" href="{{route('register')}}">Register</a>
         @else
             <ul class="menu menu-horizontal px-1">
-                <li tabindex="0">
+                <li tabindex="0" class="z-10">
                     <details>
                         <summary>{{auth()->user()->name}}</summary>
                         <ul class="p-2">
                             <li>
                                 <form action="{{route('logout')}}" method="POST">
                                     @csrf
-                                    <input type="submit" value="Logout" />
+                                    <input type="submit" value="Logout">
                                 </form>
-                            </li>
-                            <li>
-                                <a href="{{route('profile.edit')}}">Edit profile</a>
                             </li>
                         </ul>
                     </details>
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
+Route::get('/feed', [PublicController::class, 'feed'])->name('feed');
 Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
 Route::post('/post/{post}', [PublicController::class, 'comment'])->name('comment');
 Route::get('/post/{post}/like', [PublicController::class, 'like'])->name('like');
+
+Route::get('/user/{user}', [PublicController::class, 'user'])->name('user');
+
+
+
 
 // Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
 // Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
@@ -27,6 +33,11 @@ Route::get('/post/{post}/like', [PublicController::class, 'like'])->name('like')
 // Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 Route::middleware('auth')->group(function () {
     Route::resource('/admin/posts', PostController::class);
+
+    Route::get('/post/{post}/like', [PublicController::class, 'like'])->name('like');
+    Route::post('/post/{post}', [PublicController::class, 'comment'])->name('comment');
+    Route::get('/user/{user}/follow', [PublicController::class, 'follow'])->name('follow');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
